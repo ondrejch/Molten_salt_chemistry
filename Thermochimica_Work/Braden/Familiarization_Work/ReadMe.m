a9da5dc0992aed3de4c2cd1b02bccdd7f506a7e1
bclayto4UT <bkc959@my.utexas.edu>
2024-11-01 16:10:38 -0500

    Create ReadMe.m
    Creating A Shared Space for Non-Sensitive Data Collection. These files will also help Sophie and Jorge learn to create their own projects

---
+++ Thermochimica_Work/Braden/Familiarization_Work/ReadMe.m
@@ -0,0 +1,5 @@
+This file is a read me explaining the content of the directory.
+
+This directory contains the initial runs and data created while familiarizing myself with Thermochimica. These files serve two Puropses:
+1) Practice
+2) A Sanity Check
