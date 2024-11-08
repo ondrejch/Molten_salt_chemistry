aca6b7bcd472f85506fed9589faccf61d31bedfa
bclayto4UT <bkc959@my.utexas.edu>
2024-11-08 08:57:48 -0600

    Update Readme.m

--- Thermochimica_Work/Braden/Familiarization_Work/Data_Processing_Alternative/Readme.m
+++ Thermochimica_Work/Braden/Familiarization_Work/Data_Processing_Alternative/Readme.m
@@ -1,1 +1,8 @@
-## File Descriptions
+File Descriptions
+
+DataProcessing_Alt1.ipynb - This os the Jupyternotebook used to draft the phase plot generator.
+phasedataProcessor.py - This is the finalized phase plot generator, it will generate phase plots for any temprature iterated thermochemical run using the json file saved post run.
+data_structure.txt - This is a text file created to help me understand the json file structure and to learn where and how vital information is stored.
+json_explorer.ipynb - This is a notebook that was used to understand how json files store data.
+
+Generative AI Discloser: Generative AI has been used to optimize drafted code, and to generate code snipits for detailed graphs. 
